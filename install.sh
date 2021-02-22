fba67861714115ee4ed2daec5ca0ca96f2bb2824
root <power1515063@naver.com>
2021-02-22 04:37:07 +0000

    mod install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-[[ $(which sudo) = "" ]] && [[ ! $UID -eq 0 ]] && echo Need root permission; exit 1
+[[ $(which sudo) = "" ]] && [[ ! $UID -eq 0 ]] && (echo Need root permission; exit 1)
 
 git clone https://github.com/romkatv/powerlevel10k.git zsh/powerlevel10k
-git clone https://github.com/ohmyzsh/ohmyzsh.git zsh/ohmyzsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git zsh/oh-my-zsh
 
-sudo cp -r zsh/ /opt
-ln -s /opt/zsh/oh-my-zsh ~/.oh-my-zsh
+cp -r zsh/ /opt || sudo cp -r zsh/ /opt 
 ln -s /opt/zsh/zshrc ~/.zshrc
-ln -s /opt/zsh/p10k.zsh ~/.p10k.zsh
+#ln -s /opt/zsh/oh-my-zsh ~/.oh-my-zsh
+#ln -s /opt/zsh/p10k.zsh ~/.p10k.zsh
 
