fc7140aa17c8ab0b5d95982835b5598a345de383
create32 <power1515063@naver.com>
2021-02-22 13:22:37 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/romkatv/powerlevel10k.git zsh/powerlevel10k
 git clone https://github.com/ohmyzsh/ohmyzsh.git zsh/ohmyzsh
 
-sudo cp -r zsh/ /opt
+cp -r zsh/ /opt || sudo cp -r zsh/ /opt
 ln -s /opt/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -s /opt/zsh/zshrc ~/.zshrc
 ln -s /opt/zsh/p10k.zsh ~/.p10k.zsh
