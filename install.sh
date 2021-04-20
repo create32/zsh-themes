893b4e0eb184a858028162649cde47e72d3e190f
Won0 <power1515063@naver.com>
2021-04-20 15:10:08 +0900

    fix it

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@
 git clone https://github.com/romkatv/powerlevel10k.git zsh/powerlevel10k
 git clone https://github.com/ohmyzsh/ohmyzsh.git zsh/oh-my-zsh
 
-cp -r zsh/ /opt || sudo cp -r zsh/ /opt
+cp -r zsh /opt/ || sudo cp -r zsh /opt/
+if [ -f ~/.zshrc ]; then
+        mv ~/.zshrc ~/.zshrc.bak
+fi
 ln -s /opt/zsh/zshrc ~/.zshrc
