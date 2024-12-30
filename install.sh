0f4eeaf064a32b9d51ec53f6ee65fa7c46cd5816
won0-kim <won10.kim@gmail.com>
2024-12-30 16:09:13 +0900

    Update install.sh to check for zsh installation and handle existing .zsh directory

--- install.sh
+++ install.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-[[ $(which sudo) = "" ]] && [[ ! $UID -eq 0 ]] && (echo Need root permission; exit 1)
+if [ "$(which zsh)" == "" ]; then
+    echo "zsh is not installed"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
+
 
 git clone https://github.com/romkatv/powerlevel10k.git zsh/powerlevel10k
 git clone https://github.com/ohmyzsh/ohmyzsh.git zsh/oh-my-zsh
 
+# .zsh directory
+if [ -d ~/.zsh ]; then
+    echo "~/.zsh already exists"
+    exit 1
+else
+    cp -r zsh ~/.zsh
+fi
+
+# .zshrc
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.bak
 fi
-
-# for user
-cp -r zsh ~/.zsh
 ln -s ~/.zsh/zshrc_user ~/.zshrc
 
 # for global install
 # cp -r zsh /opt/ || sudo cp -r zsh /opt/
 # ln -s /opt/zsh/zshrc ~/.zshrc
-
-if [ "$(which zsh)" == "" ]; then
-    sudo apt install zsh
-fi
 
 if [ "$(which zsh)" != "" ] || [ "$SHELL" != "$(which zsh)" ]; then
     chsh -s $(which zsh)
